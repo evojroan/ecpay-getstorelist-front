e05f97ae03ef236bcfff2791846fe6bced02f1c5
R0An <roan6903@gmail.com>
2024-11-22 16:06:38 +0800

    修正 StoreCard 門市電話多餘空格

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -50,7 +50,7 @@ export function StoreCard({
         <div>門市代號(StoreId)：{StoreId}</div>
         <div>門市名稱(StoreName)：{StoreName}</div>
         <div>門市地址(StoreAddr)：{StoreAddr}</div>
-        <div>門市電話 (StorePhone)：{StorePhone}</div>
+        <div>門市電話(StorePhone)：{StorePhone}</div>
       </div>
     </>
   );
